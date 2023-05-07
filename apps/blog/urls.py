6f8a690019800e67e7992b5d320324fcbff0c1a0
Abioluz <abioluz@gmail.com>
2023-05-07 18:40:22 -0300

    #03 - Terceiro Checkpoint - Criação de templates

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('posts/<int:post_id>/',views.post_detail, name='post_detail'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Olá mundo</h1>')
+    posts = models.Post.objects.all()
+    context = {
+        'posts': posts,
+        'primeiro': posts.first(),
+
+    }
+    return render(request, 'blog/home.html', context)
+
+def post_detail(request, post_id):
+    posts = models.Post.objects.get(pk=post_id)
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_detail.html', context)
