53a70ec8b925ee13c6242d33c69a16b32d02dc82
Abioluz <abioluz@gmail.com>
2023-05-29 20:41:41 -0300

    Exemplos

---
+++ apps/exemplos/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'exemplos'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('inicio', views.get_bootstrap, name='inicio'),
+    path('containers', views.get_bootstrap_containers, name='containers'),
+    path('grids', views.get_bootstrap_grids, name='grids'),
+    path('grids2', views.get_bootstrap_grids2, name='grids2'),
+    path('grids3', views.get_bootstrap_grids3, name='grids3'),
+    path('layouts01', views.get_bootstrap_layouts01, name='layouts01'),
+    path('layouts02', views.get_bootstrap_layouts02, name='layouts02'),
+    path('layouts03', views.get_bootstrap_layouts03, name='layouts03'),
+    path('alin_hor', views.get_bootstrap_alin_hor, name='alin_hor'),
+    path('alin_ver', views.get_bootstrap_alin_ver, name='alin_ver'),
+    path('utilitarios', views.get_bootstrap_utilitarios, name='utilitarios'),
+
+]
---
+++ apps/exemplos/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'exemplos/home.html')
+
+def get_bootstrap(request):
+    return render(request, 'exemplos/01_helow_word.html')
+def get_bootstrap_containers(request):
+    return render(request, 'exemplos/02_containers.html')
+
+def get_bootstrap_grids(request):
+    return render(request, 'exemplos/04_grids.html')
+
+def get_bootstrap_grids2(request):
+    return render(request, 'exemplos/05_grids.html')
+def get_bootstrap_grids3(request):
+    return render(request, 'exemplos/06_grids.html')
+def get_bootstrap_layouts01(request):
+    return render(request, 'exemplos/07_layouts.html')
+
+def get_bootstrap_layouts02(request):
+    return render(request, 'exemplos/08_layouts.html')
+
+def get_bootstrap_layouts03(request):
+    return render(request, 'exemplos/09_layouts.html')
+
+def get_bootstrap_alin_hor(request):
+    return render(request, 'exemplos/10_alinhamento.html')
+
+def get_bootstrap_alin_ver(request):
+    return render(request, 'exemplos/11_alinhamento.html')
+
+def get_bootstrap_utilitarios(request):
+    return render(request, 'exemplos/12_utilitarios.html')
